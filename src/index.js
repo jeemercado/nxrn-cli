121f83b12690fa13cce680020f43636f49a98654
Jee Lorenz N. Mercado <mercadojee@gmail.com>
2025-03-16 15:16:45 +0800

    fix lint issues

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ import {
   removeFile
 } from './utils/index.js';
 
-const version = '2.0.7';
+const version = '2.0.8';
 const styles = {
   title: chalk.bold.cyan,
   subtitle: chalk.cyan,
--- templates/apps/mobile/src/hooks/useTextInputChangeFocus.hook.tsx
+++ templates/apps/mobile/src/hooks/useTextInputChangeFocus.hook.tsx
@@ -2,7 +2,7 @@ import React, { useRef } from 'react';
 import { TextInput } from 'react-native';
 
 export function useTextInputChangeFocus(): [React.RefObject<TextInput>, () => void] {
-  const ref = useRef<any>(null) as React.RefObject<TextInput>;
+  const ref = useRef<TextInput>(null) as React.RefObject<TextInput>;
 
   function changeFocus() {
     ref?.current?.focus();
